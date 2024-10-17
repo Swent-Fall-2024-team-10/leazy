8dc859a4167a5d04f7fe4e22416e821dede6564f
Mahdi Atallah <mahdiatallah@icloud.com>
2024-10-17 21:18:23 +0200

    implement camera (take a photo and upload to firebase)

--- app/index.tsx
+++ app/index.tsx
@@ -1,19 +1,15 @@
-import 'react-native-gesture-handler';
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { createDrawerNavigator } from '@react-navigation/drawer';
-import HomepageScreen from './screens/HomepageScreen';
-import MyRentScreen from './screens/MyRentScreen';
-import SharedElementsScreen from './screens/SharedElementsScreen';
-import SubrentScreen from './screens/SubrentScreen';
-import ReportScreen from './screens/ReportScreen';
-import SettingsScreen from './screens/SettingsScreen';
-import { registerRootComponent } from 'expo'; // Ensures it works with Expo Go
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import CameraScreen from './screens/CameraScreen';
+import CapturedMediaScreen from './screens/CapturedMediaScreen';
+
+import { registerRootComponent } from 'expo'; // Ensure you import this
 
 // portions of this code were generated with chatGPT as an AI assistant
 
-const Drawer = createDrawerNavigator();
-
+const Stack = createNativeStackNavigator();
 
 
 // Register the main component
@@ -21,16 +17,22 @@ registerRootComponent(App);
 
 
 export default function App() {
+  // Line added for testing camera
   return (
     <NavigationContainer>
-      <Drawer.Navigator initialRouteName="Home" useLegacyImplementation = {false}>
-        <Drawer.Screen name="Home" component={HomepageScreen} />
-        <Drawer.Screen name="My Rent" component={MyRentScreen} />
-        <Drawer.Screen name="Report" component={ReportScreen} />
-        <Drawer.Screen name="Shared elements" component={SharedElementsScreen} />
-        <Drawer.Screen name="Subrent" component={SubrentScreen} />
-        <Drawer.Screen name="Settings" component={SettingsScreen} />
-      </Drawer.Navigator>
-    </NavigationContainer>
+    <Stack.Navigator initialRouteName="Camera">
+      <Stack.Screen 
+        name="Camera" 
+        component={CameraScreen} 
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen 
+        name="CapturedMedia" 
+        component={CapturedMediaScreen} 
+        options={{ headerTransparent: true, headerTitle: '' }}
+      />
+    </Stack.Navigator>
+  </NavigationContainer>
   );
 }
+

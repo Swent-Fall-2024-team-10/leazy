8ed30cca75770c3efec8c58cd731c09cad816546
Darius Foodeei <darius.foodei@gmail.com>
2024-10-30 16:44:07 +0100

    added loading circle

--- app/screens/CreateNewCode.tsx
+++ app/screens/CreateNewCode.tsx
@@ -6,22 +6,27 @@ import CustomButton from "@/app/components/CustomButton";
 import { useNavigation, NavigationProp } from "@react-navigation/native"; // Import NavigationProp
 import { RootStackParamList } from "../../types/types"; // Import or define your navigation types
 import Header from "../components/Header";
-import { Clipboard, Share } from "react-native";
+import { Clipboard, Share, ActivityIndicator } from "react-native";
 
 // This screen is for the landlord to create a new code for a new tenant
 // The code will be used by the tenant to access the app for a specific residence
 export default function CodeCreationScreen() {
   const navigation = useNavigation<NavigationProp<RootStackParamList>>();
   const [code, setCode] = useState("");
+  const [loading, setLoading] = useState(false);
 
   const goBack = () => {
     navigation.goBack();
   };
 
   const createCode = () => {
-    // Create a new code for the tenant
-    // TODO: Implement code creation logic from Baudoin here
-    console.log("Create code");
+    setLoading(true); // Start loading
+
+    // Simulate a network request or code generation delay
+    setTimeout(() => {
+      setCode("JEHDHGW123123"); // Temporary code for testing
+      setLoading(false); // End loading
+    }, 2000); // Adjust delay as needed
   };
 
   const copyToClipboard = () => {
@@ -53,16 +58,16 @@ export default function CodeCreationScreen() {
 
           <CustomButton
             size="large"
-            onPress={
-              /*createCode*/ () =>
-                setCode("JEHDHGW123123") /* Temporary code for testing */
-            }
+            onPress={createCode}
             title="Create a new code"
             testID={""}
             style={styles.customButton}
           />
+
           {/* Only display the following if a code is created */}
-          {code ? (
+          {loading ? (
+            <ActivityIndicator size="large" color="#00ff88" />
+          ) : code ? (
             <>
               <Text style={styles.text}>
                 Share the following code with a tenant:

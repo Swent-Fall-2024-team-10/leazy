b182c12ede6326816ca2e25694151c70b87bd4d2
Darius Foodeei <darius.foodei@gmail.com>
2024-11-08 17:35:51 +0100

    improved styling based on figma

--- app/components/CustomButton.tsx
+++ app/components/CustomButton.tsx
@@ -56,13 +56,11 @@ const styles = StyleSheet.create({
     padding: 10,
     width: 263,
     height: 43,
-    flexShrink: 0,
     borderRadius: 25,
     borderWidth: 1,
     borderColor: '#000',
     backgroundColor: '#0F5257',
     justifyContent: 'center',
-    paddingHorizontal: 10,
   },
   contentContainer: {
     flexDirection: 'row',       // Ensures the image and text are aligned horizontally

d1a3fddd37420e64c8a598ebf919ca8160eec1d2
Mahdi Atallah <mahdiatallah@icloud.com>
2024-11-08 01:24:52 +0100

    Upload all photos at once

--- app/screens/issues_tenant/ReportScreen.tsx
+++ app/screens/issues_tenant/ReportScreen.tsx
@@ -48,6 +48,7 @@ export default function ReportScreen() {
     setIssue("");
     setDescription("");
     resetPictureList();
+    // might need to make this less grotesque as it now deletes every picture in the cache
   }
   const handleClose = () => {
     setIsVisible(true);

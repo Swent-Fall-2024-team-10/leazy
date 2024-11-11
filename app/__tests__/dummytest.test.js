98166b09d1341d1e8f7e89fe2d26753257ddfe64
Mahdi Atallah <mahdiatallah@icloud.com>
2024-11-11 17:00:18 +0100

    Add lines to see coverage report

--- app/__tests__/dummytest.test.js
+++ app/__tests__/dummytest.test.js
@@ -14,3 +14,70 @@ test('renders simple component', () => {
   const { getByText } = render(<SimpleComponent />);
   expect(getByText('Simple Test Component')).toBeTruthy();
 });
+
+
+const SimpleComponent2 = () => (
+  <View>
+    <Text>Simple Test Component</Text>
+  </View>
+);
+
+test('renders simple component', () => {
+  const { getByText } = render(<SimpleComponent2 />);
+  expect(getByText('Simple Test Component')).toBeTruthy();
+});
+
+const SimpleComponent3 = () => (
+  <View>
+    <Text>Simple Test Component</Text>
+  </View>
+);
+
+test('renders simple component', () => {
+  const { getByText } = render(<SimpleComponent3 />);
+  expect(getByText('Simple Test Component')).toBeTruthy();
+});
+
+const SimpleComponent4 = () => (
+  <View>
+    <Text>Simple Test Component</Text>
+  </View>
+);
+
+test('renders simple component', () => {
+  const { getByText } = render(<SimpleComponent4 />);
+  expect(getByText('Simple Test Component')).toBeTruthy();
+});
+
+const SimpleComponent5 = () => (
+  <View>
+    <Text>Simple Test Component</Text>
+  </View>
+);
+
+test('renders simple component', () => {
+  const { getByText } = render(<SimpleComponent5 />);
+  expect(getByText('Simple Test Component')).toBeTruthy();
+});
+
+const SimpleComponent6 = () => (
+  <View>
+    <Text>Simple Test Component</Text>
+  </View>
+);
+
+test('renders simple component', () => {
+  const { getByText } = render(<SimpleComponent6 />);
+  expect(getByText('Simple Test Component')).toBeTruthy();
+});
+
+const SimpleComponent7= () => (
+  <View>
+    <Text>Simple Test Component</Text>
+  </View>
+);
+
+test('renders simple component', () => {
+  const { getByText } = render(<SimpleComponent7 />);
+  expect(getByText('Simple Test Component')).toBeTruthy();
+});

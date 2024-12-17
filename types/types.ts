ed6de8cd39bf5332a7e11cd7c110476059f06fd6
rcharif <rayane.charifchefchaouni@epfl.ch>
2024-12-17 16:45:17 +0100

    fix navigation to chat from tenant side

--- types/types.ts
+++ types/types.ts
@@ -10,10 +10,6 @@ export type RootStackParamList = {
   ListIssues: undefined;
   WashingMachine: undefined;
   ManageWashingMachine: undefined;
-  IssueDetails: {
-    requestID: string;
-    source: "ListIssues" | "LandlordListIssues" | "LandlordDashboard";
-  };
 };
 
 export type AuthStackParamList = {

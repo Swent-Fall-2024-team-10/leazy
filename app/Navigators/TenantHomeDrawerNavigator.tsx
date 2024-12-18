4430543e97b09b63d63280c0018b5f51ae909617
Shish <rayane.charifchefchaouni@epfl.ch>
2024-12-18 19:36:16 +0000

    Improving Overall coverage for untested code (#255)
    * add:
    Numereous test suite for not tested or not fully tested files
    
    * Changes made according to the comments made for the pull request

--- app/Navigators/TenantHomeDrawerNavigator.tsx
+++ app/Navigators/TenantHomeDrawerNavigator.tsx
@@ -16,6 +16,7 @@ const Drawer = createDrawerNavigator();
 const TenantHomeDrawerNavigator = () => {
     return (
         <Drawer.Navigator
+            testID='tenant-drawer-navigator'
             initialRouteName="Home"
             useLegacyImplementation={false}
             drawerContent={(props) => <CustomDrawerContent {...props} />}
